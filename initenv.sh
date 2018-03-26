69189feef18083491e8425fa669716e14a99788a
manyang901 <gzmanyang@gmail.com>
2018-03-26 14:52:15 +0800

    feat(init): add initial env shell script

--- initenv.sh
+++ initenv.sh
@@ -4,3 +4,6 @@
 pip install --trusted-host \
 	pypi.douban.com pyinstaller \
 	-i http://pypi.douban.com/simple
+git config --global user.email "gzmanyang@gmail.com"
+git config --global user.name "manyang901"
+git config --global credential.helper store
